799b5a17f2437d1557e5633b1c1312e3dd6a614b
coolthor <coolthor@gmail.com>
2014-04-10 21:51:29 +0800

    EQ XML  transfer
    EQXML檔案中文化 depend on 字典檔

---
+++ EQUItransfer/frmEQTramsfer.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace EQUItransfer
+{
+    partial class frmEQTramsfer
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetXmlList = new System.Windows.Forms.Button();
+            this.btnTransfer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGetXmlList
+            // 
+            this.btnGetXmlList.Location = new System.Drawing.Point(12, 82);
+            this.btnGetXmlList.Name = "btnGetXmlList";
+            this.btnGetXmlList.Size = new System.Drawing.Size(180, 23);
+            this.btnGetXmlList.TabIndex = 1;
+            this.btnGetXmlList.Text = "Get File List";
+            this.btnGetXmlList.UseVisualStyleBackColor = true;
+            this.btnGetXmlList.Visible = false;
+            this.btnGetXmlList.Click += new System.EventHandler(this.btnGetXmlList_Click);
+            // 
+            // btnTransfer
+            // 
+            this.btnTransfer.Location = new System.Drawing.Point(12, 12);
+            this.btnTransfer.Name = "btnTransfer";
+            this.btnTransfer.Size = new System.Drawing.Size(435, 64);
+            this.btnTransfer.TabIndex = 2;
+            this.btnTransfer.Text = "Transfer";
+            this.btnTransfer.UseVisualStyleBackColor = true;
+            this.btnTransfer.Click += new System.EventHandler(this.btnTransfer_Click);
+            // 
+            // frmEQTramsfer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 113);
+            this.Controls.Add(this.btnTransfer);
+            this.Controls.Add(this.btnGetXmlList);
+            this.Name = "frmEQTramsfer";
+            this.Text = "EQUI中文化輔助工具 v.0.0.0.1 By Thor Lin";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetXmlList;
+        private System.Windows.Forms.Button btnTransfer;
+    }
+}
+
---
+++ EQUItransfer/frmEQTramsfer.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Web;
+using System.IO;
+using System.Data.OleDb;
+
+namespace EQUItransfer
+{
+    public partial class frmEQTramsfer : Form
+    {
+        List<string> EQxmlFilesList = new List<string>();
+        DataTable dt;
+        List<string> NodeTypeList = new List<string>();
+        public frmEQTramsfer()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string filePath = "dictionary.xlsx";
+            try
+            {
+                OleDbConnection conn = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + filePath + ";Extended Properties='Excel 12.0 Xml;HDR=YES'");
+                conn.Open();
+
+                string strSql = "Select * From [工作表1$]";
+                OleDbCommand comm = new OleDbCommand(strSql, conn);
+                OleDbDataAdapter da = new OleDbDataAdapter(comm);
+
+                if (dt == null) dt = new DataTable();
+                da.Fill(dt);
+                conn.Close();
+
+                //取的要抓的Node種類清單
+
+                DataTable distinctdt = dt.DefaultView.ToTable(true, "Node");
+                foreach (DataRow dr in distinctdt.Rows)
+                    NodeTypeList.Add("<"+dr.Field<string>("Node")+">");
+
+                btnGetXmlList_Click(null, null);
+                //btnGetXmlList.PerformClick();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnGetXmlList_Click(object sender, EventArgs e)
+        {
+            if (EQxmlFilesList != null)
+                EQxmlFilesList.Clear();
+            else
+                EQxmlFilesList = new List<string>();
+
+            XmlDocument EQxmlList = new XmlDocument();
+            if (!File.Exists("EQUI.xml"))
+            {
+                MessageBox.Show("找不到EQUI.xml!");
+                return;
+            }
+
+            EQxmlList.Load("EQUI.xml");
+            XmlNodeList xmlNodeList = EQxmlList.SelectNodes("//Include");
+
+            foreach (XmlNode xn in xmlNodeList)
+                EQxmlFilesList.Add(xn.InnerText);
+        }
+
+        private string StringToUTF8(string str)
+        {
+            string result ="";
+
+            //for (int i = 0; i < str.Length; i++)
+            //{
+
+            //    if (str[i] == 'A' || str[i] == '/')
+            //    {
+            //        result += (Microsoft.JScript.GlobalObject.escape(str[i])).Replace("%u", "&#x");
+            //    }
+            //    else if (str[i] == ':')
+            //    {
+            //        result += ':';
+            //    }
+            //    else
+            //    {
+            //        result += (Microsoft.JScript.GlobalObject.escape(str[i]) + ";").Replace("%u", "&#x");
+            //    }
+            //}
+
+            result = str;
+            return result;
+        }
+
+        private void btnTransfer_Click(object sender, EventArgs e)
+        {
+            if(!Directory.Exists("zh")) Directory.CreateDirectory("zh");
+
+            foreach (string fileName in EQxmlFilesList)
+            {
+                if (!File.Exists(fileName)) continue;
+
+                StreamReader sr = new StreamReader(fileName);
+                StreamWriter sw = new StreamWriter("zh\\" + fileName);
+               
+                    while (!sr.EndOfStream)
+                    {
+                        string str = sr.ReadLine();
+                        foreach (string node in NodeTypeList)
+                        {
+                            if (str.Contains(node))
+                            {
+                                string Value = str.Trim().Replace(node, "").Replace(node.Insert(1, "/"), "");
+                                
+                                if (Value.Contains("'")) Value =Value.Replace("'", "''");
+
+                                DataRow[] dr = dt.Select(string.Format("File='{0}' and Node ='{1}' and Source='{2}' ", fileName, node.Trim('<').Trim('>'), Value));
+                                    
+                                if (dr.Length > 0)
+                                        str = str.Replace(Value, StringToUTF8(dr[0].Field<string>("Replace")));
+
+                            }
+                        }
+                        sw.WriteLine(str);
+                    }
+                
+                sr.Close();
+                sw.Close();
+
+            }
+        }
+
+    }
+}
